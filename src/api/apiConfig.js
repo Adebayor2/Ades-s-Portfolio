2cdc96ff369632e080fcedde9ee6aba72d605272
Adebayo <adeniranadebayo27@gmail.com>
2026-07-15 18:17:04 +0100

    changes in api bse url

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,3 +1,3 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5005/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://my-portfolio-server-xdom.onrender.com';
 
 export default BASE_URL.replace(/\/$/, '');
